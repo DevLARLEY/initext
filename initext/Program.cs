12800dcff0c1b17302da0f79c17c580865272f78
DevLARLEY <121249322+DevLARLEY@users.noreply.github.com>
2024-01-29 16:40:32 +0100

    V2

--- initext/Program.cs
+++ initext/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.IO;
+using System.Text.RegularExpressions;
 
 namespace initext
 {
@@ -9,7 +7,7 @@ namespace initext
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("initext by github.com/DevLARLEY");
+            Console.WriteLine("initext by github.com/DevLARLEY\n");
             if (args.Length >= 1)
             {
                 if (File.Exists(args[0]))
@@ -20,52 +18,95 @@ namespace initext
                     ArrayList res = new ArrayList();
                     foreach (var r in pssh)
                     {
-                        int s = Convert.ToInt32(hex.Substring(r - 8, 8), 16) * 2;
-                        if (s < 100)
+                        string sysid = hex.Substring(r + 16, 32);
+                        if (sysid.Equals("EDEF8BA979D64ACEA3C827DCD51D21ED"))
                         {
-                            continue;
-                        }
-                        string f = hex.Substring(r + 56, s - 56);
-                        int p = 0;
-                        while (true)
-                        {
-                            if (p > f.Length)
+                            int s = Convert.ToInt32(hex.Substring(r - 8, 8), 16) * 2;
+                            if (s < 100)
                             {
-                                break;
+                                continue;
                             }
-                            string h = f.Substring(p, 2);
-                            if (h.Equals("48"))
+                            string f = hex.Substring(r + 56, s - 64);
+                            ArrayList t = new ArrayList();
+                            t.Add(hex.Substring(r - 8, s));
+                            int p = 0;
+                            while (p < f.Length)
                             {
-                                p += 12;
-                            }
-                            else
-                            {
-                                int s2 = Convert.ToInt32(f.Substring(p + 2, 2), 16) * 2;
-                                if (h.Equals("12"))
+                                string h = f.Substring(p, 2);
+                                switch (h)
                                 {
-                                    res.Add(new string[] { f.Substring(p + 4, s2), hex.Substring(r - 8, s), Convert.ToBase64String(Convert.FromHexString(hex.Substring(r - 8, s))) });
+                                    case "08":
+                                        p += 4;
+                                        break;
+                                    case "48":
+                                        p += 12;
+                                        break;
+                                    default:
+                                        int s2 = Convert.ToInt32(f.Substring(p + 2, 2), 16) * 2;
+                                        if (h.Equals("12"))
+                                        {
+                                            t.Add(f.Substring(p + 4, s2));
+                                        }
+                                        p += s2 + 4;
+                                        break;
                                 }
-                                p += s2 + 2;
                             }
+                            res.Add(t.ToArray());
+                        }
+                        else if (sysid.Equals("9A04F07998404286AB92E65BE0885F95"))
+                        {
+                            int s = Convert.ToInt32(hex.Substring(r - 8, 8), 16) * 2;
+                            if (s < 90)
+                            {
+                                continue;
+                            }
+                            int os = Convert.ToInt32(swap_endian(hex.Substring(r + 56, 8)), 16) * 2;
+                            string obj = hex.Substring(r + 64, os-8);
+                            ArrayList t = new ArrayList();
+                            t.Add(hex.Substring(r - 8, s));
+                            int p = 4;
+                            for (int i = 1; i <= Convert.ToInt32(swap_endian(obj.Substring(0, 4)), 16); i++)
+                            {
+                                int s2 = Convert.ToInt32(swap_endian(obj.Substring(p + 4, 4)), 16) * 2;
+                                int type = Convert.ToInt32(swap_endian(obj.Substring(p, 4)), 16);
+                                if (type == 1)
+                                {
+                                    Regex rg = new Regex("<KID ALGID=\".{6}\" VALUE=\".{24}\">");
+                                    MatchCollection matches = rg.Matches(hex_to_ascii(obj.Substring(p + 8, s2)));
+                                    foreach (var match in matches)
+                                    {
+                                        t.Add(BitConverter.ToString(Convert.FromBase64String(match.ToString().Substring(27, 24))).Replace("-", "").ToLower());
+                                    }
+                                }
+                                p += s2+8;
+                            }
+                            res.Add(t.ToArray());
                         }
                     }
+
                     if (res.Count == 0)
                     {
                         Console.WriteLine("No pssh found that contains a key id.");
                     }
                     else
                     {
-                        foreach (string[] r in res)
+                        foreach (object[] o in res)
                         {
-                            Console.WriteLine("=> Key ID: " + r[0]);
-                            Console.WriteLine("   Base16 PSSH: " + r[1]);
-                            Console.WriteLine("   Base64 PSSH: " + r[2]);
+                            if (o.Length >= 2)
+                            {
+                                Console.WriteLine("PSSH (Base64): " + Convert.ToBase64String(Convert.FromHexString((string)o[0])));
+                                for (int i = 1; i < o.Length; i++)
+                                {
+                                    Console.WriteLine("  |=> Key ID: " + o[i].ToString().ToLower());
+                                }
+                                Console.WriteLine();
+                            }
                         }
                     }
                 }
                 else
                 {
-                    Console.WriteLine("input file does not exist");
+                    Console.WriteLine("Input file does not exist.");
                 }
             }
             else
@@ -82,6 +123,26 @@ namespace initext
                 yield return minIndex;
                 minIndex = str.IndexOf(search, minIndex + search.Length);
             }
+        }
+
+        static string swap_endian(string s)
+        {
+            if (s.Length % 2 != 0)
+                s += "0";
+            string[] a = Enumerable.Range(0, s.Length).GroupBy(x => x / 2).Select(x => new string(x.Select(y => s[y]).ToArray())).ToArray();
+            Array.Reverse(a);
+            return string.Join("", a);
+        }
+
+        static string hex_to_ascii(string hex)
+        {
+            hex = hex.Replace("00", "");
+            string r = "";
+            for (int i = 0; i < hex.Length/2; i++)
+            {
+                r += (char)Convert.ToInt32(hex.Substring(i * 2, 2), 16);
+            }
+            return r;
         }
     }
 }
